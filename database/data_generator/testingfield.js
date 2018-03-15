1f0885d64e32360c139a6f72e2645d3f1e8c6564
SeSmC06 <dingpeng06@gmail.com>
2018-03-15 10:47:56 -0700

    promise function

--- database/data_generator/testingfield.js
+++ database/data_generator/testingfield.js
@@ -4,43 +4,30 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/amenities')
 
-// const insert = () => {
-//   return new Promise(resolve => setTimeout(resolve, 300))
-// }
-
-// const generateOne = async idx => {
-//   await insert()
-//   generate(idx)
-// }
-// await
 let tracker = 0
 const delay = () => new Promise(resolve => setTimeout(resolve, 300))
 
 const populateTwoK = async () => {
   const guest = []
-  for (let i = 1; i < 100000; i++) {
-    // await delay
-    console.time()
+  for (let i = 1; i < 100; i++) {
     guest.push(generate(i))
   }
-  return Promise.resolve(guest)
-  // return new Promise(resolve =>
-  //   setTimeout(() => {
-  //     resolve()
-  //   })
-  // )
+  return db.insertData(guest)
+    .then(() => console.log('saved 100 data'))
+    .catch(e => console.log('failed to save'))
 }
 
 const insertTwentyThousand = async () => {
   await populateTwoK().then(data => {
     db.insertData(data).then(() => {
-      // console.log(tracker++)
-      // if (tracker === 10) {
-      //   mongoose.disconnect()
-      //   console.timeEnd()
-      // }
-      mongoose.disconnect()
-      console.timeEnd()
+      tracker++
+      if (tracker === 10) {
+        mongoose.disconnect()
+        console.log('finalized')
+      }
+      // mongoose.disconnect()
+
+      console.log('inserted 100')
     })
   })
 }
@@ -49,13 +36,14 @@ const checkToStop = () => {
   return Promise.resolve(tracker === 100)
 }
 
-// const inertThisMany = async () => {
-//   for (let i = 0; i < 10; i++) {
-//     console.time()
-//     await insertTwentyThousand()
-//   }
-// }
+const inertThisMany = async () => {
+  for (let i = 0; i < 100; i++) {
+    // console.time()
+    await insertTwentyThousand()
+  }
+}
 
-// inertThisMany()
-insertTwentyThousand()
+inertThisMany()
+
+// insertTwentyThousand()
 // console.log(100 * 20000)
