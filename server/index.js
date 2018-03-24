23aa9f747c39abff93a10e327de13e04ed0bd4f1
SeSmC06 <dingpeng06@gmail.com>
2018-03-24 09:36:19 -0700

    custome script for stress test

--- server/index.js
+++ server/index.js
@@ -1,36 +1,65 @@
-/*  using mongoose 
-const mongoose = require('mongoose')
-const Amenities = require('../database/models/amenities')
-mongoose.connect('mongodb://localhost/amenities') */
-require('newrelic')
 const cluster = require('cluster')
-const express = require('express')
-const cors = require('cors')
-const pgData = require('./postgres_server')
-const redis = require('redis')
-const app = express()
-const port = process.env.PORT || 3001
-const client = redis.createClient(6379)
-const cache = require('./redis_cache')
 
-app.use(cors())
-app.use('/amenities/:id', express.static(__dirname + '/../client'))
-app.use(express.static(__dirname + '/../client'))
+require('newrelic')
 
+cache = (request, response, next) => {
+  const id = request.params.id
+  client.get(id, (error, data) => {
+    if (error) {
+      throw error
+    }
+    if (data !== null) {
+      const result = JSON.parse(data)
+      response.send(result)
+    } else {
+      next()
+    }
+  })
+}
 
-// for postgres use
-app.get('/amenities/:id/amenities/', cache, async (req, res) => {
-  const { id } = req.params
-  // Amenities.getAmenityById(id).then(result => res.json(result))
-  try {
-    const amenities = await pgData(id)
-    client.setex(req.params.id, 300, JSON.stringify(amenities))
-    res.json(amenities)
-  } catch (e) {
-    res.status(500).send('Invalid Parm')
+// using cluster
+
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  var cpuCount = require('os').cpus().length
+
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork()
   }
-})
 
-app.listen(port, function() {
-  console.log(`listening on port ${port}`)
-})
+  // Listen for dying workers
+  cluster.on('exit', function(worker) {
+    // Replace the dead worker, we're not sentimental
+    console.log('Worker %d died :(', worker.id)
+    cluster.fork()
+  })
+} else {
+  const express = require('express')
+  const cors = require('cors')
+  const pgData = require('./postgres_server')
+  const redis = require('redis')
+  const app = express()
+  const port = process.env.PORT || 3001
+  const client = redis.createClient(6379)
+
+  app.use(cors())
+  app.use('/amenities/:id', express.static(__dirname + '/../client'))
+  app.use(express.static(__dirname + '/../client'))
+
+  app.get('/amenities/:id/amenities/', cache, async (req, res) => {
+    const { id } = req.params
+    // Amenities.getAmenityById(id).then(result => res.json(result))
+    try {
+      const amenities = await pgData(id)
+      client.setex(req.params.id, 300, JSON.stringify(amenities))
+      res.json(amenities)
+    } catch (e) {
+      res.status(500).send('Invalid Parm')
+    }
+  })
+
+  app.listen(port, function() {
+    console.log(`listening on port ${port}`)
+  })
+}
