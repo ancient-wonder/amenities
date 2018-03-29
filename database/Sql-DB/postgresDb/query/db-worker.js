b92349767a424024d48beac5e384016319b73393
SeSmC06 <dingpeng06@gmail.com>
2018-03-29 11:58:51 -0700

    seed

--- database/Sql-DB/postgresDb/query/db-worker.js
+++ database/Sql-DB/postgresDb/query/db-worker.js
@@ -22,7 +22,7 @@ const dbtables = pgp({
 // 37 minutes total for 7 table insertion
 
 const batchInsert = async () => {
-  for (let i = 1; i < 1000000; i += 10000) {
+  for (let i = 1; i < 10000000; i += 10000) {
     await insertIntoUniqueTable(dbtables, pgp, i, shipHeader, createShipObj)
     await insertIntoGenericTable(dbtables, pgp, i, usersHeader, createUserObj)
     await insertIntoGenericTable(
